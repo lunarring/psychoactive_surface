3274b597b3c0d63036a160a95c3385caac2dcfc1
DGX <aloktyus@uos.de>
2024-01-25 13:27:04 +0000

    first version coupler: modulation for prompt blending and latent noise

--- main_coupling_render.py
+++ main_coupling_render.py
@@ -17,14 +17,18 @@ import lunar_tools as lt
 import numpy as np
 from PIL import Image
 from lunar_tools.comms import OSCSender, OSCReceiver
+from lunar_tools.control_input import MidiInput
 
-mainOSCReceiver = OSCReceiver('10.20.17.122', port_receiver=8005)
+from prompt_blender import PromptBlender
+
+akai_lpd8 = MidiInput(device_name="akai_midimix")
+mainOSCReceiver = OSCReceiver('10.20.17.122', port_receiver=8004)
 
 torch.set_grad_enabled(False)
 torch.backends.cuda.matmul.allow_tf32 = False
 torch.backends.cudnn.allow_tf32 = False
 
-use_maxperf = False
+use_maxperf = True
 use_image_mode = False
 
 if use_image_mode:
@@ -63,29 +67,60 @@ renderer = lt.Renderer(width=sz[1], height=sz[0])
 latents = torch.randn((1,4,64,64)).half().cuda() # 64 is the fastest
 latents_additive = torch.randn((1,4,64,64)).half().cuda() # 64 is the fastest
 
+prompts = ['cat', 'cat and dog']
 
-# Iterate over blended prompts
-# blended = blender.blend_prompts(blended_prompts[0], blended_prompts[1], 0)
-# prompt_embeds, negative_prompt_embeds, pooled_prompt_embeds, negative_pooled_prompt_embeds = blended
+# Iterate over blended promptsblender = PromptBlender(pipe)
+
+prompts = []
+prompts.append('red strange carpet full of wool')
+prompts.append('green tower full of mossy reird plants')
+
+blender = PromptBlender(pipe)
 
 #%%#
+n_steps = 2500
+blended_prompts = blender.blend_sequence_prompts(prompts, n_steps)
+
+
 while True:
-    osc_data = mainOSCReceiver.get_last_value("/env1")
-    print(f'data {osc_data}')
     
     # if data is not None:
     #     print(f'data {len(data)}')    
     
     # latents_modulated = latents + osc_data * latents_additive * 5*1e-1
     # latents_modulated = latents + torch.roll(latents_additive, int(osc_data*100)) * 0.2
-    latents_modulated = torch.roll(latents_additive, int(osc_data*20))
+    # latents_modulated = torch.roll(latents_additive, int(osc_data*20))
     
-    image = pipe(prompt='cat', guidance_scale=0.0, num_inference_steps=1, latents=latents_modulated).images[0]
+    for i in range(len(blended_prompts) - 1):
+        osc_data = mainOSCReceiver.get_last_value("/env1")
+        print(f'data {osc_data}')        
         
-    # Render the image
-    image = np.asanyarray(image)
-    image = np.uint8(image)
-    renderer.render(image)
+        # fract = float(i) / (len(blended_prompts) - 1)
+        fract = 0
+        
+        # modulation gain
+        a0 = akai_lpd8.get("A0", val_min=0, val_max=1, val_default=0)
+        a1 = akai_lpd8.get("A1", val_min=0, val_max=10, val_default=0)
+        
+        i_b = i
+        i_b += (int(osc_data*10000*a0)-0.5)
+        i_b = int(np.clip(i_b, 0, len(blended_prompts)-2))
+        
+        height = latents.shape[2]
+        vert_ramp = torch.linspace(0,1,height).to(latents.device)
+        latents_modulated = latents + latents_additive * vert_ramp.reshape([1,1,height,1]) * a1
+        latents_modulated = latents_modulated.half()
+        
+        blended = blender.blend_prompts(blended_prompts[i_b], blended_prompts[i_b+1], fract)
+    
+        prompt_embeds, negative_prompt_embeds, pooled_prompt_embeds, negative_pooled_prompt_embeds = blended
+            
+        image = pipe(guidance_scale=0.0, num_inference_steps=1, latents=latents_modulated, prompt_embeds=prompt_embeds, negative_prompt_embeds=negative_prompt_embeds, pooled_prompt_embeds=pooled_prompt_embeds, negative_pooled_prompt_embeds=negative_pooled_prompt_embeds).images[0]
+        
+        # Render the image
+        image = np.asanyarray(image)
+        image = np.uint8(image)
+        renderer.render(image)
         
         
     
